d2246183aa445c7232513b8dd16ac9502d2af1d4
Shijin2k3 <shijinkumar2003@gmail.com>
2025-02-19 16:48:42 +0530

    created register User and its routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,21 @@
-import User from "../models/userModel";
+const User=require("../models/userModel");
 
-export const registerUser=async(req,res,next)=>{
+
+exports.registerUser=async(req,res,next)=>{
+   try{
     const {email,username,password}=req.body;
     const user=await User.create({email,username,password});
+    res.status(200).json({
+        success:true,
+        message:"Api performed successfully"
+    })
+   }catch(err){
+    console.log(err)
+    res.status(400).json({
+        success:false,
+        message:"user not created",
+        error:err.message
+    })
+
+   }
 }
