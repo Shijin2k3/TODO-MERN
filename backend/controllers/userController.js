4fef5419d638102c0c1e435eecdb57463e49466e
Shijin2k3 <shijinkumar2003@gmail.com>
2025-02-22 14:23:38 +0530

    initialized loginUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,16 +1,17 @@
 const User=require("../models/userModel");
+const bcrypt=require('bcryptjs')
 
-
+//register user 
 exports.registerUser=async(req,res,next)=>{
    try{
     const {email,username,password}=req.body;
-    const user=await User.create({email,username,password});
+    const hashPassword=bcrypt.hashSync(password)
+    const user=await User.create({email,username,password:hashPassword});
     res.status(200).json({
         success:true,
         user
     })
    }catch(err){
-    console.log(err)
     res.status(400).json({
         success:false,
         message:"user already exist",
@@ -18,4 +19,36 @@ exports.registerUser=async(req,res,next)=>{
     })
 
    }
+}
+
+//login user
+exports.loginUser=async(req,res,next) => {
+    try{
+        const user= await User.findOne({email:req.body.email});
+        if(!user){
+            res.status(400).json({
+                success:false,
+                message:"Please Register first"
+            })
+        }
+        const isPasswordCorrect= bcrypt.compareSync(req.body.password,user.password);
+        if(!isPasswordCorrect){
+            res.status(400).json({
+                success:false,
+                message:"Incorrect Password"
+            })
+        }
+        const {password,...others}=user._doc;
+        res.status(200).json({
+            success:true,
+            others
+        })
+
+    }catch(err){
+        res.status(400).json({
+            success:false,
+            message:"",
+            error:err.message
+        }) 
+    }
 }
