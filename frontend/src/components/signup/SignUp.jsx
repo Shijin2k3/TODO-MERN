5357d22d6a6e76152435e5e14da939461ff01c2d
Shijin2k3 <shijinkumar2003@gmail.com>
2025-03-10 20:39:20 +0530

    initialized changes in backend and performed register user function

--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios';
+
 
 export const SignUp = () => {
+ const [inputs,setInputs]=useState({
+  email:"",
+  username:"",
+  password:""
+ })
+ 
+
+  const handleChange =(e) =>{
+    const {name,value}=e.target;
+    setInputs({...inputs,[name]:value})
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    await axios.post("http://localhost:8000/api/v1/register",inputs).then(response => {
+      console.log(response.data);
+      setInputs({
+        email:"",
+        username:"",
+        password:""
+       })
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+    setInputs({
+      email:"",
+      username:"",
+      password:""
+     })
+    //  console.log(inputs)
+    //  toast.success("Registered Successfully")
+  }
   return (
     <div className='w-[100%] h-[88vh] px-10'>
+      <ToastContainer />
       <div className=' '>
         <div className='flex justify-between '>
           <div className='w-[100%] p-5 my-10 flex justify-center items-center '>
@@ -11,19 +47,19 @@ export const SignUp = () => {
               <form className='w-[35%] flex flex-col' action="">
                 <h1 className='my-3 font-medium'>If you don't have an Account. Please SignUp... </h1>  
                 <input
-                 className='p-2 my-3 border-2 border-gray-500 rounded-2xl outline-none' type="email" name="email"
+                 className='p-2 my-3 border-2 border-gray-500 rounded-2xl outline-none' type="email" name="email" onChange={handleChange}
 
-                  placeholder='Enter Your Email' autoComplete='off'
+                  placeholder='Enter Your Email' autoComplete='off' value={inputs.email}
                 />
                 <input
-                 className='p-2 my-3 border-2 border-gray-500 rounded-2xl outline-none' type="text"  name="username"
-                  placeholder='Enter Your Username' autoComplete='off' 
+                 className='p-2 my-3 border-2 border-gray-500 rounded-2xl outline-none' type="text"  name="username" onChange={handleChange}
+                  placeholder='Enter Your Username' autoComplete='off' value={inputs.username}
                 />
                 <input
-                 className='p-2 my-3 border-2 border-gray-500 rounded-2xl outline-none' type="password" name="password"
-                  placeholder='Enter Your Password' autoComplete='off'
+                 className='p-2 my-3 border-2 border-gray-500 rounded-2xl outline-none' type="password" name="password" onChange={handleChange}
+                  placeholder='Enter Your Password' autoComplete='off' value={inputs.password}
                 />
-                <button type='submit' className= 'my-3 bg-black font-bold text-xl text-gray-300 py-4 rounded-2xl cursor-pointer hover:bg-gray-950'>SignUp</button>
+                <button type='submit' onClick={handleSubmit} className= 'my-3 bg-black font-bold text-xl text-gray-300 py-4 rounded-2xl cursor-pointer hover:bg-gray-950'>SignUp</button>
               </form>
             </div>
           </div>
